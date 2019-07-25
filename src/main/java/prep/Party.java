a31584f43a89476df50508c005c49fe40af9c03a
Vasudev Koneru <vasudev.koneru1@t-mobile.com>
2019-07-24 22:38:42 -0700

    Push

---
+++ src/main/java/prep/Party.java
@@ -0,0 +1,33 @@
+package prep;
+
+public class Party {
+
+    String partyName;
+    String leader;
+
+    /**
+     * Gets partyName
+     *
+     * @return value of partyName
+     */
+    public String getPartyName() {
+        return partyName;
+    }
+
+    public void setPartyName(String partyName) {
+        this.partyName = partyName;
+    }
+
+    /**
+     * Gets leader
+     *
+     * @return value of leader
+     */
+    public String getLeader() {
+        return leader;
+    }
+
+    public void setLeader(String leader) {
+        this.leader = leader;
+    }
+}
